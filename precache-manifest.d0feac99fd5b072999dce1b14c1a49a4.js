e94a771a459e9cba7bb7965cecab843fac77145f
iguzmanl <ivonneaguzmanl@gmail.com>
2021-02-17 17:28:49 -0800

    Updates

--- precache-manifest.d0feac99fd5b072999dce1b14c1a49a4.js
+++ precache-manifest.d0feac99fd5b072999dce1b14c1a49a4.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5d4766c0cec3f08b1843dbc57d393dd",
+    "revision": "8531e5bc20f2d1e694bbdbe1185da346",
     "url": "/index.html"
   },
   {
-    "revision": "757067fbcab61a83fec2",
-    "url": "/static/css/main.5e0f739b.chunk.css"
+    "revision": "430a702f88deace68ea0",
+    "url": "/static/css/main.f8b3b95d.chunk.css"
   },
   {
-    "revision": "a7058d46fe9b6d9d6d7c",
-    "url": "/static/js/2.0afe4725.chunk.js"
+    "revision": "6e4de12023de03bd0069",
+    "url": "/static/js/2.c19d4c53.chunk.js"
   },
   {
-    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/static/js/2.0afe4725.chunk.js.LICENSE.txt"
+    "revision": "1a3cc71403f332d098a2b9cdd7c4c36d",
+    "url": "/static/js/2.c19d4c53.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "757067fbcab61a83fec2",
-    "url": "/static/js/main.47e813f3.chunk.js"
+    "revision": "430a702f88deace68ea0",
+    "url": "/static/js/main.d3cb7dd2.chunk.js"
   },
   {
-    "revision": "b17457f05973c0e67538",
-    "url": "/static/js/runtime-main.4fa04a44.js"
+    "revision": "85d46b49da0e5d2bfeec",
+    "url": "/static/js/runtime-main.b85b21b4.js"
   },
   {
     "revision": "7f42d8488652eb250af2f484d377dbee",
